d77978ad293bda70f268e1c47bfaa5eafe281ae5
Omira <Omira@192.168.1.224>
2019-08-19 10:25:24 -0500

    master commit

---
+++ src/Introduction.java
@@ -0,0 +1,23 @@
+
+public class Introduction {
+	 public static void main(String[]args) {
+		 
+		 System.out.println(myNameIs());
+		 System.out.println(myFavoriteColor());
+		 System.out.println(lotteryWinner());
+		 
+	 }
+	 
+	 private static String myNameIs() {
+		 return "My name is Sydney M. Porter.";
+	 }
+	 
+	 private static String myFavoriteColor() {
+		 return "My favorite color is purple.";
+	 }
+	 
+	 private static String lotteryWinner() {
+		 return "If I won the lottery, I would start a trust fund for my children and invest the rest.";
+	 }
+
+}
